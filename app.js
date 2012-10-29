30d91ae9e009c1414174fad0f8b9d42d573935b2
Pascal Garber <jumplink@gmail.com>
2012-10-29 15:16:54 +0100

    Inital stuff

---
+++ app.js
@@ -0,0 +1,15 @@
+// sublime: tab_size 2; translate_tabs_to_spaces true
+
+var config = require('json-fs').open(__dirname+"/config/general.json");
+console.log(config.heritage);
+console.log(config.magento);
+var heritage = require('heritage')(config.heritage);
+var magento = require('magento')(config.magento);
+
+magento.auto.catalog.product.info("021-198-009/B", config.magento.store_view[0].code, function (error, result) {
+  console.log(result);
+});
+
+heritage.get_product("021-198-009/B", function(data) {
+  console.log(data);
+});
