0165e8364cce11bafdf4461eac88d0307d607430
Pascal Garber <jumplink@gmail.com>
2012-11-10 15:11:00 +0100

    check each project from heritage for availability in magento

--- app.js
+++ app.js
@@ -5,14 +5,28 @@ var heritage = require('heritage')(config.heritage);
 var magento = require('magento')(config.magento);
 var fs = require('fs');
 
-// magento.auto.catalog.product.info("021-198-009/B", config.magento.store_view[0].code, function (error, result) {
-//   console.log(result);
+// magento.manual.init(function(err) {
+//   magento.auto.catalog.product.info("021-198-009/B", config.magento.store_view[0].code, function (error, result) {  
+//     console.log(result);
+//   });
 // });
 
-heritage.auto.catalog.product.info("252-711-629", function(data) {
-  console.log(data);
+
+heritage.auto.catalog.product.list(function(data) {
+  magento.manual.init(function(err) {
+    for (var i = data.CODE.length - 1; i >= 0; i--) {
+      magento.auto.catalog.product.available(data.CODE[i], config.magento.store_view[0].code, function (available, sku) {
+        console.log("sku: "+sku+" "+available);
+      });
+    };
+  });
 });
 
+// heritage.auto.catalog.product.info("252-711-629", function(data) {
+//   console.log(data);
+// });
+
+// true
 // magento.auto.catalog.product.available("021-198-009/B", config.magento.store_view[0].code, function (available) {
 //   console.log(available);
 // });
@@ -22,23 +36,3 @@ heritage.auto.catalog.product.info("252-711-629", function(data) {
 // });
 
 // var products = {};
-
-// var csv = require('csv');
-// csv()
-// .from.stream(fs.createReadStream(__dirname+'/parts-DE-1.csv'))
-// .transform( function(data){
-//   data.unshift(data.pop());
-//   return data;
-// })
-// .on('record', function(data,index){
-//   //console.log('#'+index+' '+JSON.stringify(data));
-//   products[data[2]]=data;
-//   console.log(data);
-// })
-// .on('end', function(count){
-//   //console.log('Number of lines: '+count);
-//   //console.log(products);
-// })
-// .on('error', function(error){
-//   console.log(error.message);
-// });
